7f2ee74fe1650879b17664b613719005befc6645
Emil1577 <emilronquillo@gmail.com>
2023-03-08 12:46:55 -0800

    working on resolvers

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,60 @@
+const { Tech, Matchup } = require('../models');
+
+const resolvers = {
+  Query: {
+    tech: async () => {
+      return Tech.find({});
+    },
+    matchups: async (parent, { _id }) => {
+      const params = _id ? { _id } : {};
+      return Matchup.find(params);
+    },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+//need to work on this
+    saveBook: async (parent, { authors, description, bookId, image, link, title, }, context) => {
+      if (context.book) {
+        const book = await book.create({
+          thoughtText,
+          thoughtAuthor: context.user.username,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { thoughts: thought._id } }
+        );
+
+        return thought;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+
+
+
+
+  }
+
+module.exports = resolvers;
